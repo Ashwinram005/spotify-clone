b43167fca7f42bd27a2e9495da75844b5240c3e5
Ashwinram005 <ashwinrammohan2005@gmail.com>
2024-12-13 23:00:08 +0530

    updated

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -20,10 +20,20 @@ const Player = () => {
         <div className="flex items-center gap-5">
             <p>1:06</p>
             <div className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
-                <hr className="h-1 border-none bg-green-800 rounded-full"/>
+                <hr className="h-1 w-0 border-none bg-green-800 rounded-full"/>
             </div>
             <p>3:20</p>
         </div>
+      </div>
+      <div className="hidden lg:flex items-center gap-2 opacity-75">
+        <img src={assets.plays_icon} alt="playsicon" className="w-4"/>
+        <img src={assets.mic_icon} alt="micicon" className="w-4"/>
+        <img src={assets.queue_icon} alt="queueicon" className="w-4"/>
+        <img src={assets.speaker_icon} alt="speakericon" className="w-4"/>
+        <img src={assets.volume_icon} alt="volumeicon" className="w-4"/>
+        <div className="w-20 bg-slate-50 h-1 rounded"></div>
+        <img src={assets.mini_player_icon} alt="miniplayericon" className="w-4"/>
+        <img src={assets.zoom_icon} alt="zoomicon" className="w-4"/>
       </div>
     </div>
   )
